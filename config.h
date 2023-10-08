bdeb7594024ff4988e694cff547fc2055f3505dc
Henry Savary-Jackson <floandicoot@gmail.com>
2023-10-08 11:23:24 +0200

    fucked up my colour scheme

--- config.h
+++ config.h
@@ -8,15 +8,15 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=15" };
 
 static const char dmenufont[]       = "monospace:size=15";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#666666";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#111111";
+static const char col_gray4[]       = "#111111";
+static const char col_cyan[]        = "#CC2222";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray2 , col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
